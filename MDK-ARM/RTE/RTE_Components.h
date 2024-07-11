3ffe32c986d118d56e6d3962799c5b5d1bd043e3
Weijie Liu <1079338392@qq.com>
2024-07-11 18:38:41 +0800

    Add files via upload

---
+++ MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'program for gait' 
+ * Target:  'program for gait' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
---
+++ MDK-ARM/program for gait/startup_stm32f10x_md.d
@@ -0,0 +1,1 @@
+program for gait\startup_stm32f10x_md.o: ..\Core\Src\startup_stm32f10x_md.s
